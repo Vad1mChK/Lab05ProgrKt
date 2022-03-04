e262e865713cdc0f8712b3273ccec15eab87776f
Vad1mChK <snusmumrike2@gmail.com>
2022-03-04 09:59:32 +0300

    deleted all

--- lab-client/src/ru/vad1mchk/progr/lab05/client/datatypes/Chapter.kt
+++
@@ -1,7 +0,0 @@
-package ru.vad1mchk.progr.lab05.client.datatypes
-
-/**
- * Class that represents chapters.
- * Each character has its name, parent legion, and space marines
- */
-data class Chapter(val name: String, val parentLegion: String?, val marinesCount: Int)
--- lab-client/src/ru/vad1mchk/progr/lab05/client/util/DataField.kt
+++
@@ -1,5 +0,0 @@
-package ru.vad1mchk.progr.lab05.client.util
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FIELD)
-annotation class DataField()
